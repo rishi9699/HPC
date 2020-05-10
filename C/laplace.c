c6973c016e79220a8e2d3a371ca1dfd957174ef8
rishi9699 <rishi9699@gmail.com>
2020-05-10 20:38:35 +0530

    Cleaned the code and made a few optimizations Code is 10x faster than the previous upload

--- C/laplace.c
+++ C/laplace.c
@@ -1,103 +1,129 @@
-/*
-Laplace solver on a unit square
-The actual function here is x^2 - y^2
-
-Iterating over the sub-2d-array using a single variable, without OpenMP
-*/
-
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
+
+#define ROWS 500
+#define COLUMNS 500
+
+#define ITERATIONS 10000
+
+
+void boundaryconditions(float *);
+void duplicate_grids(float *, float *);
+void perform_iterations(float*);
+float sum_of_squares(float *);
+
 
 int main()
 {
-    int r=500, c=500;  //number of grid points
-    float a[r][c];
+    /* Allocating memory equivalent to two grids  */
+    float *grid1 = calloc(ROWS*COLUMNS*2, sizeof(float));
+    float *grid2 = grid1 + (ROWS*COLUMNS);
 
-    //Initializing array
-    for(int x=0; x<c; x++)
-    for(int y=0; y<r; y++)
-    a[y][x]=0;
+    boundaryconditions(grid1);
 
-    //Imposing boundary conditions
-    for(int x=0; x<c; x++)
-    {
-        a[0][x]=(float)x/(c-1)*x/(c-1);
-        a[r-1][x]=(float)x/(c-1)*x/(c-1)-1;
-    }
-     for(int y=0; y<r; y++)
-    {
-        a[y][0]=-(float)y/(r-1)*y/(r-1);
-        a[y][r-1]=1-(float)y/(r-1)*y/(r-1);
-    }
-
-
-    //Another array for carrying iterations
-    float b[r][c];
-    for(int y=0; y<r; y++){
-        //printf("\n");
-    for(int x=0; x<c; x++){
-        b[y][x]=a[y][x];
-        //printf("%.5f ", a[y][x]);
-    }}
-
-    //printf("\n");
-
-    float *ptr1=(float *)a;
-    float *ptr2=(float *)b;
-
-    float sum1=0, sum2=0;
-    int counter = 10000;   //number of iterations
-
-    float *pos1=NULL, *pos2=NULL, *temp=NULL;  //To be used inside the loop during iteration
+    duplicate_grids(grid1, grid2);
 
     clock_t tic = clock();
-   
-    while(counter > 0)
-    {
-        //sum1=sum2;
-        //sum2=0;
-        temp=ptr1;  //interchanging the reference to the arrays
-        ptr1=ptr2;
-        ptr2=temp;
-
-        for(int i=0;i<((r-2)*(c-2));i++)
-        {
-            pos1=ptr1+((i/(r-2))+1)*c+(i%(c-2))+1;
-            pos2=ptr2+((i/(r-2))+1)*c+(i%(c-2))+1;
-            *(pos2)=0.25*(*(pos1-c)+*(pos1+c)+*(pos1-1)+*(pos1+1));
-
-            /* printf("%.2f ",*pos2);
-            if((i+1)%(c-2) == 0)
-            printf("\n"); */
-            
-            //sum2=sum2+ ((*pos2) * (*pos2));
-        }
-        
-        //printf("\n");
-        counter--;
-        //printf("%.2f, %.2f, %.4f\n", sum1, sum2, sum1-sum2);
-    }
-
-    clock_t toc = clock(); //stop counter
-
-    //SUM OF SQUARES OF THE ELEMENTS FOR THE 2 ARRAYS
-    for(int i=0;i<((r-2)*(c-2));i++)
-    {
-        pos1=ptr1+((i/(r-2))+1)*c+(i%(c-2))+1;
-        pos2=ptr2+((i/(r-2))+1)*c+(i%(c-2))+1;
-        
-        sum1=sum1+ ((*pos1) * (*pos1));
-        sum2=sum2+ ((*pos2) * (*pos2));
-    }
-
+    perform_iterations(grid1);
+    clock_t toc = clock();
 
     
-    printf("%.8f, %.8f, %.8f\n", sum1, sum2, sum1-sum2);   //the difference in square sums
-    printf("%.15f\n", (double)(toc - tic) / CLOCKS_PER_SEC); //time taken
+    float sum1=0, sum2=0;
+    sum1=sum_of_squares(grid1);
+    sum2=sum_of_squares(grid2);
 
-    /*int x=55;
-    int y=73;
-    printf("%.5f, %.5f", a[y][x], (float)x/(c-1)*x/(c-1) - (float)y/(r-1)*y/(r-1));*/ //To check the true value vs. computed value
+
+    free(grid1);
+
+    printf("Individual sum of squares: %.8f, %.8f\nDifference: %.8f\n", sum1, sum2, sum1-sum2);
+    printf("Time taken for iterations: %.15f\n", (double)(toc - tic) / CLOCKS_PER_SEC);
 
     return 0;
+}
+
+
+float sum_of_squares(float *grid)
+{
+    /*Excluding points corresponding to the boundary conditions*/
+    float sum=0;
+    for(int i=1;i<ROWS-1;i++)
+    {
+        for(int j=1;j<COLUMNS-1;j++)
+        {
+            sum = sum + grid[i*COLUMNS + j] * grid[i*COLUMNS + j];
+        }
+    }
+    return sum;
+}
+
+
+void boundaryconditions(float *grid1)
+{
+    /* Boundary conditions at x=0,x=1,y=0,y=1 for function x^2-y^2*/
+    for(int j=0; j<COLUMNS; j++)
+    {
+        grid1[j]=(float)j/(COLUMNS-1)*j/(COLUMNS-1);
+        grid1[(ROWS-1)*COLUMNS+j]=(float)j/(COLUMNS-1)*j/(COLUMNS-1)-1;
+    }
+     for(int i=0; i<ROWS; i++)
+    {
+        grid1[i*COLUMNS]=-(float)i/(ROWS-1)*i/(ROWS-1);
+        grid1[i*COLUMNS + ROWS-1]=1-(float)i/(ROWS-1)*i/(ROWS-1);
+    }
+}
+
+
+void duplicate_grids(float *grid1, float *grid2)
+{
+    for(int i=0; i<ROWS; i++)
+    {
+        for(int j=0; j<COLUMNS; j++)
+        grid2[i*COLUMNS + j]=grid1[i*COLUMNS + j];
+    }
+    
+}
+
+
+void perform_iterations(register float *grid1)
+{
+    int counter = ITERATIONS/2;
+    float *grid1end = grid1 + (ROWS-1) * COLUMNS;
+    float *grid2end = grid1 + (2*ROWS - 1) * COLUMNS;
+
+    while( (--counter) > 0)
+    {
+        
+        /*Setting up variables for odd iteration*/
+        register float *pos_grid1= grid1 + COLUMNS;
+        register float *pos_grid2= grid1 + (ROWS+1)*COLUMNS;
+
+        register float *row_end= pos_grid1 + COLUMNS - 1;
+
+        for(; row_end<grid1end ;row_end+=COLUMNS)
+        {
+            for(;(++pos_grid1)<row_end;)
+            {
+                *(++pos_grid2)= 0.25f * (pos_grid1[-COLUMNS] + pos_grid1[-1] + pos_grid1[1] + pos_grid1[COLUMNS]);
+            }
+            pos_grid1+=1;
+            pos_grid2+=2;
+        }
+
+        /*Setting up variables for even iteration*/
+        pos_grid1= grid1 + COLUMNS;
+        pos_grid2= grid1 + (ROWS+1)*COLUMNS;
+
+        row_end= pos_grid2 + COLUMNS - 1;
+
+        for(; row_end<grid2end ;row_end+=COLUMNS)
+        {
+            for(;(++pos_grid2)<row_end;)
+            {
+                *(++pos_grid1)= 0.25f * (pos_grid2[-COLUMNS] + pos_grid2[-1] + pos_grid2[1] + pos_grid2[COLUMNS]);
+            }
+            pos_grid2+=1;
+            pos_grid1+=2;
+        }
+    }   
 }
