9f5c19962d5e2f76091ad39df249b5f6781901f0
rishi9699 <rishi9699@gmail.com>
2020-06-14 21:32:23 +0530

    Cleaned and optimized the python code Used the numpy add function instead of iterating over each element

--- python/lap_numba.py
+++ python/lap_numba.py
@@ -2,71 +2,49 @@ import time
 import numpy as np
 from numba import njit
 
-r=500
-c=500
-
-a=np.zeros((r,c))
-
-# for x in range(c):
-#     for y in range(r):
-#         a[y][x]=1;
-    
-for x in range(c):
-    a[0][x]=x/(c-1)*x/(c-1)
-    a[r-1][x]=x/(c-1)*x/(c-1)-1
-
-for y in range(r):
-    a[y][0]=-y/(r-1)*y/(r-1);
-    a[y][r-1]=1-y/(r-1)*y/(r-1);
-    
-b=np.zeros((r,c))
-
-for y in range(r):
-    for x in range(c):
-        b[y][x]=a[y][x];
-        
-sum1=0
-sum2=0
-counter=10000
-#temp=np.zeros((r,c))
-
-#start=time.perf_counter()
-
-@njit
-def runloop(counter,a,b):
-    while counter>0:
-        #sum1=sum2
-        #sum2=0
-        temp=a
-        a=b
-        b=temp
-        
-        for y in range(1,r-1):
-            #print("\n")
-            for x in range(1,c-1):
-                b[y][x]=0.25*(a[y-1][x] + a[y][x-1] + a[y][x+1] + a[y+1][x])
-                #sum2=sum2+(b[y][x] * b[y][x])
-                
-        counter-=1 
+ROWS = 500
+COLUMNS = 500
+ITERATIONS = 10000
 
 
+@njit(parallel=True)
+def perform_iterations(counter, grid1, grid2):
+    counter /= 2
+    while (counter) > 0:
+        grid2[1:ROWS-1, 1:COLUMNS-1] = 0.25*np.add(np.add(grid1[2:ROWS, 1:COLUMNS-1], grid1[0:ROWS-2, 1:COLUMNS-1]),
+                                                   np.add(grid1[1:ROWS-1, 2:COLUMNS], grid1[1:ROWS-1, 0:COLUMNS-2]))
+        grid1[1:ROWS-1, 1:COLUMNS-1] = 0.25*np.add(np.add(grid2[2:ROWS, 1:COLUMNS-1], grid2[0:ROWS-2, 1:COLUMNS-1]),
+                                                   np.add(grid2[1:ROWS-1, 2:COLUMNS], grid2[1:ROWS-1, 0:COLUMNS-2]))
+        counter -= 1
 
-start=time.perf_counter()
-runloop(counter,a,b)
-stop=time.perf_counter() 
+
+def boundary_conditions(grid):
+    for i in range(COLUMNS):
+        grid[0][i] = i/(COLUMNS-1)*i/(COLUMNS-1)
+        grid[ROWS-1][i] = i/(COLUMNS-1)*i/(COLUMNS-1)-1
+
+    for j in range(ROWS):
+        grid[j][0] = -j/(ROWS-1)*j/(ROWS-1)
+        grid[j][ROWS-1] = 1-j/(ROWS-1)*j/(ROWS-1)
 
 
-for y in range(1,r-1):
-    for x in range(1,c-1):
-        sum2=sum2+(b[y][x] * b[y][x])
-        sum1=sum1+(a[y][x] * a[y][x])
+grid1 = np.zeros((ROWS, COLUMNS))
+grid2 = np.zeros((ROWS, COLUMNS))
 
-  
-# for y in range(r):
-#     print()
-#     for x in range(c):
-#         print(b[y][x], end=' ')
-        
-        
-print(sum1,sum2,sum1-sum2)
-print(stop-start)
+boundary_conditions(grid1)
+boundary_conditions(grid2)
+
+start_time = time.perf_counter()
+perform_iterations(ITERATIONS, grid1, grid2)
+stop_time = time.perf_counter()
+
+sum1 = 0
+sum2 = 0
+
+for j in range(1, ROWS-1):
+    for i in range(1, COLUMNS-1):
+        sum2 = sum2+(grid2[j][i] * grid2[j][i])
+        sum1 = sum1+(grid1[j][i] * grid1[j][i])
+
+print(sum1, sum2, sum1 - sum2)
+print(stop_time - start_time)
